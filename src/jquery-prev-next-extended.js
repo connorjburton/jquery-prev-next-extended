918eed65e4fe86810c6c1a1d278e6c40697669ec
Connor Burton <connorjburton@gmail.com>
2016-06-24 13:11:44 +0100

    Initial commit, only next supported, no npm support

---
+++ src/jquery-prev-next-extended.js
@@ -0,0 +1,20 @@
+(function($)
+{
+    var ref = $.fn.next;
+    $.fn.next = function(num)
+    {
+    	if(typeof num === 'undefined') num = 1;
+        var ret = ref.apply(this, arguments);
+
+        if(num > 1 && ret.length) {
+        	var all = ret.nextAll();
+        	
+        	if(all.length >= num - 1) {
+        		var idx = ret.index();
+        		ret = ret.add(all.slice(0, num - 1));
+        	}
+        }
+
+        return ret;
+    };
+})(jQuery);
